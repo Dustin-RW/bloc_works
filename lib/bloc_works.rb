fb29e57202be9ac8b4f5e988aa571cb0b660bd91
Dustin <dustinwaggoner@comcast.net>
2017-03-20 10:43:19 -0700

    controller configuration implemented, running into gem error

--- lib/bloc_works.rb
+++ lib/bloc_works.rb
@@ -1,4 +1,5 @@
 require "bloc_works/version"
+require "bloc_works/controller"
 
 module BlocWorks
   class Application
@@ -6,6 +7,6 @@ module BlocWorks
     def call(env)
       [200, {'Content-Type' => 'text/html'}, ["Hello Blocheads!"]]
     end
-    
+
   end
 end
