5fc993206969c40014b312963c87dab0cc471352
Dustin <dustinwaggoner@comcast.net>
2017-03-22 09:37:53 -0700

    controller modifications made

--- lib/bloc_works.rb
+++ lib/bloc_works.rb
@@ -6,6 +6,12 @@ module BlocWorks
     #call returns an array containing an HTTP status code, an HTTP header, and the text to display in the Browser
     def call(env)
       [200, {'Content-Type' => 'text/html'}, ["Hello Blocheads!"]]
+      # response = fav_icon(env)
+      #
+      # if response.nil?
+      # end
+      # if requesting favicon.ico - return fav_icon response
+      # otherwise, execute action on controller and return result
     end
 
   end
